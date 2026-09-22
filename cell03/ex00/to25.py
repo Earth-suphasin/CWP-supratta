4f33d2a1efb58445c833a99611ef3890572167b2
Earth-suphasin <su4925079@gmail.com>
2026-09-23 01:35:09 +0700

    add cell03 ex00

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,10 @@
+print("Enter a number less than 25")
+number = int(input())
+
+if number > 25:
+    print("Error")
+else:
+    while number <= 25:
+        print("Inside the loop, my variable is", number)
+        number += 1
+        
